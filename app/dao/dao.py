27abb7a4ba60a33354bb97229dbb996569babebf
Ubuntu <ubuntu@ip-172-31-12-140.ap-south-1.compute.internal>
2024-09-23 20:23:46 +0000

    minor chamges in dao

--- app/dao/dao.py
+++ app/dao/dao.py
@@ -3,23 +3,26 @@ from sqlalchemy.future import select
 from app.models.models import Video
 from app.schemas.videos import VideoCreate
 
-async def add_video(session: AsyncSession, video_create: VideoCreate) -> Video:
-    video = Video(
-        name=video_create.name,
-        size=video_create.size,
-        path=video_create.path,
-    )
-    session.add(video)
-    await session.commit()
-    await session.refresh(video)
-    return video
+class VideoDAO:
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
+    async def add_video(self, video_create: VideoCreate) -> Video:
+        video = Video(
+            name=video_create.name,
+            size=video_create.size,
+            path=video_create.path,
+        )
+        self.session.add(video)
+        await self.session.commit()
+        await self.session.refresh(video)
+        return video
 
-async def search_videos(session: AsyncSession, name: str = None, size: int = None):
-    query = select(Video)
-    if name:
-        query = query.filter(Video.name.ilike(f"%{name}%"))
-    if size:
-        query = query.filter(Video.size == size)
-    result = await session.execute(query)
-    return result.scalars().all()
+    async def search_videos(self, name: str = None, size: int = None):
+        query = select(Video)
+        if name:
+            query = query.filter(Video.name.ilike(f"%{name}%"))
+        if size:
+            query = query.filter(Video.size == size)
+        result = await self.session.execute(query)
+        return result.scalars().all()
