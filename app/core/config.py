58ac3105a1b926a03b8b0259e38d9b1b59458fee
sanyogpatel-tecblic <sanyogpatel.tecblic@gmail.com>
2024-09-23 19:03:24 +0530

    initial setup

--- app/core/config.py
+++ app/core/config.py
@@ -2,12 +2,27 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import redis.asyncio as redis
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost/mi_db"
+# Load environment variables from .env file
+load_dotenv()
+
+# Fetch the variables from the environment
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+REDIS_URL = os.getenv("REDIS_URL")
+
+# Construct the database URL
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Setup SQLAlchemy
 Base = declarative_base()
-
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-# Redis cache setup
-redis_cache = redis.from_url("redis://localhost")
+# Setup Redis cache
+redis_cache = redis.from_url(REDIS_URL)
